e9a96b44fc42e0c60d543d914d42a286982bbcd9
abhigyak47 <124084882+abhigyak47@users.noreply.github.com>
2023-04-24 17:25:35 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -9,23 +9,24 @@
 #include <fstream>
 #include <map>
 #include <algorithm>
+#include <string>
 #include "httplib.h"
+#include <vector>
 
 using namespace httplib;
 using namespace std;
 
 const int port = 5005;
 
+
 void addMessage(string username, string message, map<string,vector<string>> &messageMap) {
 	/* iterate through users adding message to each */
 	string jsonMessage = "{\"user\":\""+username+"\",\"message\":\""+message+"\"}";
 	for (auto userMessagePair : messageMap) {
-		username = userMessagePair.first;
-		messageMap[username].push_back(jsonMessage);
+	username = userMessagePair.first;
+	messageMap[username].push_back(jsonMessage);
 	}
 }
-
-
 
 
 void addUser(string username, string password, string email, map<string,string> &userMap) {
@@ -34,102 +35,105 @@ void addUser(string username, string password, string email, map<string,string> 
 }
 
 
-
-
-
 string getMessagesJSON(string username, map<string,vector<string>> &messageMap) {
 	/* retrieve json list of messages for this user */
 	bool first = true;
 	string result = "{\"messages\":[";
-	for (string message :  messageMap[username]) {
-		if (not first) result += ",";
-		result += message;
-		first = false;
+	for (string message : messageMap[username]) {
+	if (not first) result += ",";
+	result += message;
+	first = false;
 	}
 	result += "]}";
 	messageMap[username].clear();
 	return result;
 }
 
+
 int main(void) {
-  Server svr;
-  int nextUser=0;
-  map<string,vector<string>> messageMap;
-  map<string,string> userMap;
-  map<string,string> userEmail;
-	
-  /* "/" just returnsAPI name */
-  svr.Get("/", [](const Request & /*req*/, Response &res) {
-    res.set_header("Access-Control-Allow-Origin","*");
-    res.set_content("Chat API", "text/plain");
-  });
-
+ Server svr;
+ int nextUser=0;
+ map<string,vector<string>> messageMap;
+ map<string,string> userMap;
+ map<string,string> userEmail;
 
 	
-svr.Get(R"(/chat/join/(.*)/(.*))", [&](const Request& req, Response& res) {
+ /* "/" just returnsAPI name */
+ svr.Get("/", [](const Request & /*req*/, Response &res) {
  res.set_header("Access-Control-Allow-Origin","*");
- string username = req.matches[1];
- string password = req.matches[2];
- string email = userEmail[username];
- string userDetails = "{\"user\":\""+username+"\",\"pass\":\""+password+"\",\"email\":\""+email+"\"}";
-	
+ res.set_content("Chat API", "text/plain");
+ });
+
+
+ 
+ svr.Get(R"(/chat/register/(.*)/(.*)/(.*))", [&](const Request& req, Response& res) {
+	res.set_header("Access-Control-Allow-Origin","*");
+ 	string username = req.matches[1];
+	string email = req.matches[2];
+	string password = req.matches[3];
+ 	string result;
+ 	vector<string> empty;
+ if (messageMap.count(username) or messageMap.count(email) or password.length() < 7){
+ result = "{\"status\":\"registrationfailure\"}";
+ } else {
+ messageMap[username]= empty;
+	userEmail[username] = email;
+	addUser(username , password, email , userMap);
+ result = "{\"status\":\"success\",\"user\":\"" + username + "\",\"email\":\"" + email + "\",\"pass\":\"" + password + "\"}";
+
+ }
+ res.set_content(result, "text/json");
+ });
+ 
+ 
+
+ svr.Get(R"(/chat/join/(.*)/(.*))", [&](const Request& req, Response& res) {
+ res.set_header("Access-Control-Allow-Origin","*");
+ 	string username = req.matches[1];
+	string password = req.matches[2];
+	string email = userEmail[username];
+	string userDetails= "{\"user\":\""+username+"\",\"pass\":\""+password+"\",\"email\":\""+email+"\"}";
+
  string result;
  // Check if user with this name and password exists
- if (userDetails == userMap[username]){
+ if (userDetails== userMap[username]){
  result = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
- cout << username << " joins" << endl;
+	activeUsers[username] = "this user is active";
+	cout << username << " joins" << endl;
  } else {
  result = "{\"status\":\"failure\"}";
  }
  res.set_content(result, "text/json");
+	getUserList(userMap);
  });
-	
-	
 
-   svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
-    res.set_header("Access-Control-Allow-Origin","*");
+ 
+
+ svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
+ res.set_header("Access-Control-Allow-Origin","*");
 	string username = req.matches[1];
 	string message = req.matches[2];
 	string result; 
-	
-    if (!messageMap.count(username)) {
-    	result = "{\"status\":\"baduser\"}";
+ if (!messageMap.count(username)) {
+ result = "{\"status\":\"baduser\"}";
 	} else {
-		addMessage(username,message,messageMap);
-		result = "{\"status\":\"success\"}";
+	addMessage(username,message,messageMap);
+	result = "{\"status\":\"success\"}";
 	}
-    res.set_content(result, "text/json");
-  });
-  
-	
-svr.Get(R"(/chat/register/(.*)/(.*)/(.*))", [&](const Request& req, Response& res) {
-    res.set_header("Access-Control-Allow-Origin","*");
-	string username = req.matches[1];
-	string email = req.matches[2];
-	string password = req.matches[3];
-	string result; 
-	vector<string> empty;
-	cout<<messageMap[password];
-	if (messageMap.count(password)<7){
-		result= "{\"status\":\"registrationFailed\"}";
-	} else{
-		messageMap[username]=empty;
-		userEmail[username]=email;
-		addUser(username, password, email, userMap);
-		result= "{\"status\":\"success\"}";
-	}
-	res.set_content(result,"text/json");
-});
-			
-	
-   svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
-    string username = req.matches[1];
-    res.set_header("Access-Control-Allow-Origin","*");
-    string resultJSON = getMessagesJSON(username,messageMap);
-    res.set_content(resultJSON, "text/json");
-  });
-  
-  cout << "Server listening on port " << port << endl;
-  svr.listen("0.0.0.0", port);
+ res.set_content(result, "text/json");
+ });
+ 
+ 
+ svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
+ string username = req.matches[1];
+ res.set_header("Access-Control-Allow-Origin","*");
+ string resultJSON = getMessagesJSON(username,messageMap);
+ res.set_content(resultJSON, "text/json");
+ });
+ 
+
+ 
+ cout << "Server listening on port " << port << endl;
+ svr.listen("0.0.0.0", port);
 
 }
